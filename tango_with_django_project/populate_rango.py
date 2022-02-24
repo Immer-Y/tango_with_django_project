6c18d1c16a53b561900a3fd4713d55f4cb2ac154
vincent972123 <392025574@qq.com>
2022-02-24 00:02:59 +0000

    chapter6

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -16,23 +16,31 @@ def populate():
     # # through each data structure, and add the data to our models.
     python_pages = [
         {'title': 'Official Python Tutorial', 
-         'url':'http://docs.python.org/3/tutorial/',},
+         'url':'http://docs.python.org/3/tutorial/',
+         'views':10},
         {'title':'How to Think like a Computer Scientist', 
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views':20},
         {'title':'Learn Python in 10 Minutes', 
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views':30}]
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views':40},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/',
+         'views':50},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+         'url':'http://www.tangowithdjango.com/',
+         'views':60}]
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',
+         'views':70},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ]
+         'url':'http://flask.pocoo.org',
+         'views':80}]
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
             'Django': {'pages': django_pages, 'views':64, 'likes':32},
             'Other Frameworks': {'pages': other_pages, 'views':32, 'likes':16} }
@@ -43,7 +51,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -56,8 +64,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0] 
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.likes = likes
+    c.views = views 
     c.save()
     return c
 
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,15 +1,21 @@
-from turtle import title
-from unicodedata import category
-from django import views
+
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+        
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
         
     def __str__(self): 
         return self.name
@@ -23,9 +29,5 @@ class Page(models.Model):
     def __str__(self): 
         return self.title
     
-class PageAdmin(models.Model):
-    list_display = ('title', 'category', 'url')
-    
-    def __list__(self):
-        return  self.list_display
+
     
